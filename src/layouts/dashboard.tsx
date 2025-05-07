8d4f2ee3ffb05234e23736750f558d02335d94f2
william <sibueawilliam99@gmail.com>
2025-05-07 16:34:24 +0700

    update padding

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from 'react-router';
 const Dashboard = () => {
     return(
         <>
-          <div className="px-4 py-2 bg-white rounded mx-4">
+          <div className="px-4 py-2 bg-white rounded mx-4 my-4">
             <div className='grid grid-cols-12'>
               <div className='col-span-2 h-screen'>
                     <ul className='my-4'>
@@ -13,7 +13,7 @@ const Dashboard = () => {
                         <li className='px-6 py-3 hover:text-blue-200 mx-2 rounded text-sm'><Link to="/help">help</Link></li>
                     </ul>
               </div>
-              <div className='col-span-10'>
+              <div className='col-span-10 my-6'>
                 <Outlet />
               </div>
             </div>
